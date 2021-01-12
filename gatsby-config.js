b1806721f8aa82e581ea3de141ffe5e3f6e462f9
giordancltr <giordancltr@gmail.com>
2021-01-11 23:16:07 -0800

    Actual fix?

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,22 +7,3 @@ module.exports = {
     'gatsby-plugin-netlify-cms'
   ]
 };
-
-
-collections:
-  - name: "blog"
-    label: "Blog"
-    folder: "src/blog"
-    create: true
-    slug: "{{year}}-{{month}}-{{day}}-{{slug}}"
-    fields:
-      - label: "Title"
-        name: "title"
-        widget: "string"
-      - label: "Publish Date"
-        name: "date"
-        widget: "datetime"
-      - label: "Body"
-        name: "body"
-        widget: "markdown"
-
