1f92ddb849a277136e5fd8e7068845fdd86c6bdd
giordancltr <giordancltr@gmail.com>
2021-01-11 23:08:16 -0800

    Add initial configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,28 @@
 module.exports = {
   siteMetadata: {
     title: 'The Coffee Blog'
-  }
+  },
+
+  plugins: [
+    'gatsby-plugin-netlify-cms'
+  ]
 };
+
+
+collections:
+  - name: "blog"
+    label: "Blog"
+    folder: "src/blog"
+    create: true
+    slug: "{{year}}-{{month}}-{{day}}-{{slug}}"
+    fields:
+      - label: "Title"
+        name: "title"
+        widget: "string"
+      - label: "Publish Date"
+        name: "date"
+        widget: "datetime"
+      - label: "Body"
+        name: "body"
+        widget: "markdown"
+
---
+++ src/components/Throwaway.js
@@ -0,0 +1,9 @@
+import styles from './Throwaway.modules.css'
+export default function Sample() {
+  return (
+    <div>
+      <header className={styles.header}>Header</header>
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  );
+}
