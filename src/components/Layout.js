daa140b82eb22d87eb69897c67a6a8c5618286bd
giordancltr <giordancltr@gmail.com>
2021-01-12 21:53:53 -0800

    Add blog post pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styles from './Layout.module.css';
 
 export default function Layout({ children }) {
@@ -6,7 +7,7 @@ export default function Layout({ children }) {
     <div>
       <header id={styles.header}>
         <div id={styles.inner}>
-          <h1>Gio's Blogspot</h1>
+          <h1><Link to="/">Gio's Blogspot</Link></h1>
         </div>
       </header>
       <main id={styles.main}>
