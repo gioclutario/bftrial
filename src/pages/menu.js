89f9789393af7813023ba2d384079ca4a12c2b91
giordancltr <giordancltr@gmail.com>
2021-02-22 22:05:39 -0800

    working update

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,43 +1,43 @@
-// import React from 'react';
+import React from 'react';
 
-// import Layout from '../components/Layout';
-// import MenuCategory from '../components/MenuCategory';
+import Layout from '../components/Layout';
+import MenuCategory from '../components/MenuCategory';
 
-// import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 
-// import styles from './menu.module.css';
+import styles from './menu.module.css';
 
-// export default function Menu() {
-//   const data = useStaticQuery(graphql`
-//     {
-//       markdownRemark(frontmatter: { contentKey: { eq: "menu" } }) {
-//         frontmatter {
-//           title
-//           categories {
-//             name
-//             items {
-//               name
-//               description
-//               price
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `); 
+export default function Menu() {
+  const data = useStaticQuery(graphql`
+    {
+      markdownRemark(frontmatter: { contentKey: { eq: "menu" } }) {
+        frontmatter {
+          title
+          categories {
+            name
+            items {
+              name
+              description
+              price
+            }
+          }
+        }
+      }
+    }
+  `);
 
-//   return (
-//     <Layout>
-//       <div id={styles.main}>
-//         <h1>{data.markdownRemark.frontmatter.title}</h1>
-//         <div id={styles.menu}>
-//           {data.markdownRemark.frontmatter.categories.map(category => (
-//             <MenuCategory
-//               key={category.name}
-//               category={category} />
-//           ))}
-//         </div>
-//       </div>
-//     </Layout>
-//   );
-// }
+  return (
+    <Layout>
+      <div id={styles.main}>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <div id={styles.menu}>
+          {data.markdownRemark.frontmatter.categories.map(category => (
+            <MenuCategory
+              key={category.name}
+              category={category} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+}
